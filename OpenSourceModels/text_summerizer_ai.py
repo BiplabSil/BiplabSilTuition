ca574cd65c6da9531b7bc5d8e6e4d67eca01fc86
Biplab Sil <biplab.sil.1998.01.16@gmail.com>
2025-08-24 14:07:18 +0530

    day-4

--- OpenSourceModels/text_summerizer_ai.py
+++ OpenSourceModels/text_summerizer_ai.py
@@ -7,6 +7,19 @@ summerizerPipe = pipeline("summarization", model="facebook/bart-large-cnn",max_n
 #article
 article = "Dr. A.P.J. Abdul Kalam, born in 1931 in Rameswaram, Tamil Nadu, was an Indian aerospace scientist and the 11th President of India (2002–2007) known for his pivotal role in India's space and missile programs, which earned him the titles 'Missile Man of India' and 'People/'s President'. He served as Project Director for India's first indigenous Satellite Launch Vehicle and led the development of the AGNI and PRITHVI missiles before becoming President. Kalam, a vegetarian and devout Muslim, rose from humble origins to become an inspirational figure, dedicating his life to science and education until his passing in 2015."
 
-response = summerizerPipe("summerize the article given as {}".format(article))
+#response = summerizerPipe("summerize the article given as {}".format(article))
 
-print(response[0]['summary_text'])
+#print(response[0]['summary_text'])
+import gradio as gr
+
+def summerize(text):
+    res = summerizerPipe(text)
+    return res[0]['summary_text']
+demo = gr.Interface(
+    fn=summerize,
+    inputs="text",
+    outputs="text",
+    title= "Text Summerizer",
+    description="Provide Text To Summerize"
+)
+demo.launch()
